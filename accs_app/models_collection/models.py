c56fa822a96e713375fadf7264a8977bffea1e25
JBinkowski <jan.binkowski96@gmail.com>
2024-07-28 16:13:35 +0200

    Added workflow script as artificats component

--- accs_app/models_collection/models.py
+++ accs_app/models_collection/models.py
@@ -2,22 +2,22 @@ from os.path import join
 from django.db import models
 from django.conf import settings
 from django.utils.timezone import now
-from tinymce import models as tinymce_models
 
 
 # Create your models here.
 class ModelInstance(models.Model):
     def path(self, file):
-        return join(settings.MODELS_ROOT, str(self.name), file)
+        return join(settings.ARTIFACTS_ROOT, str(self.name), file)
 
     name = models.CharField(max_length=50, unique=True)
     creation_date = models.DateTimeField(default=now)
-    description = tinymce_models.HTMLField(null=True, blank=True)
+    description = models.CharField(max_length=1024, null=True, blank=True)
 
     model = models.FileField(upload_to=path, max_length=255)
     scaler = models.FileField(upload_to=path, max_length=255)
     imputer = models.FileField(upload_to=path, max_length=255)
     anomaly_detector = models.FileField(upload_to=path, max_length=255)
+    preprocessing_flow = models.FileField(default="", upload_to=path, max_length=255)
 
     def __str__(self):
         return self.name
