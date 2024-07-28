c60d3a22ca60eff74c7436a773da29282246007d
JBinkowski <jan.binkowski96@gmail.com>
2024-07-28 16:34:18 +0200

    Fixed

--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,7 @@
-cd accs_app/ || exit
-
 echo "----- R cache ------ "
 Rscript sesame_requirements.R
+
+cd accs_app/ || exit
 
 echo "----- Start celery ------ "
 python3.10 -m celery -A accs_app worker --detach --loglevel=info --concurrency=2
@@ -17,4 +17,4 @@ echo "----------- Add superuser --------- "
 python3.10 manage.py createsuperuser --no-input
 
 echo "----------- Start app --------- "
-python3.10 py
+python3.10  -m gunicorn 'accs_app.wsgi' --bind=0.0.0.0:8000
