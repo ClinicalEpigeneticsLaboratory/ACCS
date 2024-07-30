48525296e18b7aff9b2f3fce5d41eaf994826ec4
JBinkowski <jan.binkowski96@gmail.com>
2024-07-30 18:45:22 +0200

    Updated

--- accs_app/accs_app/settings.py
+++ accs_app/accs_app/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", default="XXXX")
+if not os.getenv("SECRET_KEY"):
+    raise Exception("missing SECRET_KEY")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", default=True)
+DEBUG = os.getenv("DEBUG", default=False) == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "app.epi-can.com"]
 
 # Application definition
 INSTALLED_APPS = [
