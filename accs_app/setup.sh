9815450711c4ea914cb5ff0c8550b3242da6d941
JBinkowski <jan.binkowski96@gmail.com>
2024-12-18 12:48:35 +0100

    Nginx update

--- accs_app/setup.sh
+++ accs_app/setup.sh
@@ -13,4 +13,11 @@ echo "----------- Add superuser --------- "
 python3.10 manage.py createsuperuser --no-input
 
 echo "----------- Add superuser --------- "
-python3.10 -m gunicorn 'accs_app.wsgi' --bind=0.0.0.0:8000
+if [[ -n "$SSL_CERT" && -n "$SSL_KEY" ]]; then
+  echo "Running gunicorn with cert/key files"
+  python3.10 -m gunicorn 'accs_app.wsgi' --bind=0.0.0.0:8000 --certfile=/etc/ssl/certs/$SSL_CERT --keyfile=/etc/ssl/certs/$SSL_KEY
+
+else
+  echo "Files cert/key not available"
+  python3.10 -m gunicorn 'accs_app.wsgi' --bind=0.0.0.0:8000
+fi
