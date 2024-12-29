6ec507d2dcb631c6aca494b3a1b951a8f1ed76a7
JBinkowski <jan.binkowski96@gmail.com>
2024-12-30 00:35:59 +0100

    Added workers and timeout for gunicorn

--- accs_app/setup.sh
+++ accs_app/setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-echo "----- Start celery ------ "
-python3.10 -m celery --app=accs_app.celery worker --pool=eventlet --logfile="celery.log" --autoscale=3,2 -E &
-
 echo "----- Collect static files ------ "
-python3.10 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "----------- Apply migrations --------- "
-python3.10 manage.py makemigrations
-python3.10 manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 echo "----------- Add superuser --------- "
-python3.10 manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
 
 echo "----------- Add superuser --------- "
-python3.10 -m gunicorn 'accs_app.wsgi' --bind=0.0.0.0:8000
+python -m gunicorn 'accs_app.wsgi' --bind=0.0.0.0:8000 --timeout 60 --workers=2
