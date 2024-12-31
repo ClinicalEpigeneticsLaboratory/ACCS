94cdbb4d242d2be030c1a08c36f3d1d13d2a4096
JBinkowski <jan.binkowski96@gmail.com>
2024-12-31 20:45:19 +0100

    Added new filed to user registration form

--- accs_app/users/forms.py
+++ accs_app/users/forms.py
@@ -21,7 +21,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "institution", "password1", "password2"]
 
     def clean_acceptance(self):
         acceptance = self.cleaned_data.get("acceptance")
