891cb54a994de17f25c2d176d360cd13d370f861
JBinkowski <jan.binkowski96@gmail.com>
2025-01-07 20:56:22 +0100

    Fixed favicon error

--- accs_app/accs_app/settings.py
+++ accs_app/accs_app/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "icons")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 
 MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
